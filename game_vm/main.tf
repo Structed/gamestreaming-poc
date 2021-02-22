9d9c7aa0bdda5dd6fd66094fd1453dc0d1e3ae33
Johannes Ebner <Structed@users.noreply.github.com>
2021-02-22 22:21:07 +0100

    extracted VM networ stuff

--- game_vm/main.tf
+++ game_vm/main.tf
@@ -1,26 +1,4 @@
 
-
-resource "azurerm_public_ip" "ip_game" {
-  name                = "ipGame"
-  sku                 = "Basic"
-  resource_group_name = var.rg.name
-  location            = var.rg.location
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_interface" "nic_game" {
-  name                = "nic"
-  location            = var.rg.location
-  resource_group_name = var.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip_game.id
-    subnet_id                     = var.subnet.id
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "vm_game" {
   name                = "vmgame"
   resource_group_name = var.rg.name
