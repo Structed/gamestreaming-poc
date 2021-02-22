a29798239460978b0dbba6754e0657023e082124
Johannes Ebner <Structed@users.noreply.github.com>
2021-02-22 17:17:05 +0100

    initial

---
+++ main.tf
@@ -0,0 +1,81 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-boo-poc"
+  location = "westus2"
+}
+
+resource "azurerm_public_ip" "ip_game" {
+  name                = "ipGame"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_virtual_network" "network_game" {
+  name                = "network_game"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet_game" {
+  name                 = "subnet_game"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.network_game.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic_game" {
+  name                = "nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    private_ip_address_allocation = "Dynamic"
+    sku                           = "Basic"
+    public_ip_address_id          = azurerm_public_ip.ip_game.id
+    subnet_id                     = azurerm_subnet.subnet_game.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm_game" {
+  name                = "vm_game"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_NC8as_T4_v3"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic_game.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
